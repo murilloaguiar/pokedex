94a6b2d650e73e5b65927591f47e85ca9eeddc2c
Murillo Aguiar <aguiar_murillo@outlook.com>
2022-04-28 11:30:32 -0300

    Estrutura inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <div class="container">
+      <div class="row mt-2">
+         <div class="col mb-2">palco</div>
+         <div class="col mb-2">pokedex</div>
+      </div>
+   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+   name: 'Home',
+   components: {
+      
+   }
 }
 </script>
